b3d3948cf7b4e7cecbbb3ff0e26a96fb6cc10e6a
Daniel Godwin <danielg7@gmail.com>
2018-03-17 19:36:47 -0600

    Updated testing

--- run_tests.R
+++ run_tests.R
@@ -1,9 +1,20 @@
 library(testthat)
 library(shinytest)
 
-#test_that("Application works", {
-#  expect_pass(testApp("FireWeatherExplorer", compareImages = FALSE))
-#  expect_pass(testApp("06_tabsets", compareImages = FALSE))
-#})
+# Function: readInWeather -------------------------------------------------
 
+test_that("readInWeather fails with non-numeric years - start", {
+  expect_error(readInWeather(StationID = "RFRC2", County = "Larimer",Start = "ABC",End = 2015))
+})
 
+test_that("readInWeather fails with non-numeric years - end", {
+  expect_error(readInWeather(StationID = "RFRC2", County = "Larimer",Start = 2015, End = "ABC"))
+})
+
+test_that("readInWeather fails if end is greater than start year", {
+  expect_error(readInWeather(StationID = "RFRC2", County = "Larimer",Start = 2015, End = 2011),"Start year must be less than end year.")
+})
+
+test_that("readInWeather fails if StationID is invalid", {
+  expect_error(readInWeather(StationID = "ABCD1", County = "Larimer", Start = 2010, End = 2011))
+})
