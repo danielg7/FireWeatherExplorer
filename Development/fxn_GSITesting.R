808d6ff9b4ef52eb6df3f753c3cb04c1f8588798
Daniel Godwin <danielg7@gmail.com>
2018-04-25 18:57:14 -0600

    Streamlined GSI testing in development

--- Development/fxn_GSITesting.R
+++ Development/fxn_GSITesting.R
@@ -1,133 +1,141 @@
 library("geosphere")
 library("zoo")
+library("dplyr")
+library("lubridate")
 
-testWx_cleaned$DayLength <- geosphere::daylength(lat = as.numeric(stationMetadata$STATION$LATITUDE),
+testWx <- readInWeather(StationID = "KS25",
+                        County = "McKenzie",
+                        Start = 2014,
+                        End = 2014)
+
+testWx_cleaned <- fxn_weatherCleaner(testWx)
+
+
+
+testWx_cleaned$DayLength <- geosphere::daylength(lat = 47.663722,
                      doy = yday(x = testWx_cleaned$DateTime))
 
 ggplot(data = testWx_cleaned, aes(x = DayOfYear, y = DayLength))+
   geom_point()+
   facet_wrap(~Year)
 
-
-# Calculate Vapor Pressure Deficit ----------------------------------------
-
-# T is temp in Celcius
-TempC <- (testWx_cleaned$Temp - 32) / 1.8
-
-es <- 0.6108 * exp(17.27 * TempC / (TempC + 237.3))
-
-# Actual vapor pressure:
-
-ea <- testWx_cleaned$RH / 100 * es
-
-# Vapor Pressure Deficit
-
-testWx_cleaned$VPD <- ea - es
-
-get.es <- function(temp){
-  TempC <- (temp - 32) / 1.8
+calcGSI <- function(DateTime, Temp, RH, Latitude)
+{
+  originalDF <- data.frame(DateTime,Temp,RH)
   
-  es <- 6.11 * exp((2.5e6 / 461) * (1 / 273 - 1 / (273 + TempC)))
-  return(es)
-}
-
-get.vpd <- function(rh, temp){
-  ## calculate saturation vapor pressure
-  es <- get.es(temp)
-  ## calculate vapor pressure deficit
-  vpd <- ((100 - rh) / 100) * es * 100
-  return(vpd)
-}
-
-testWx_cleaned$VPD <- get.vpd(rh = testWx_cleaned$RH,
-                              temp = testWx_cleaned$Temp)
-
-iGSI_hourly <- function(MinimumTemp, VPD, Photoperiod){
+  workingDF <- data.frame(DateTime,Temp,RH)
+  workingDF$Year <- lubridate::year(DateTime)
+  workingDF$Yday <- lubridate::yday(x = workingDF$DateTime)
+  workingDF$DayOfYear <- as.Date(paste("2000-",format(workingDF$DateTime, "%j")), "%Y-%j")
+  workingDF$DayLength <- geosphere::daylength(lat = Latitude,
+                                              doy = workingDF$Yday)
+  
+  workingDF <- filter(workingDF, hour(DateTime) == 13)
   
   
-  # Calculate ITmin
-  
-  if(MinimumTemp >= 41 & !is.na(MinimumTemp))
-    iTmin <- 1
-  if(MinimumTemp < 41 & MinimumTemp > 28 & !is.na(MinimumTemp))
-    iTmin <- (MinimumTemp - 28)/(41 - 28)
-  if(MinimumTemp <= 28 & !is.na(MinimumTemp))
-    iTmin <- 0
-  if(is.na(MinimumTemp))
-    iTmin <- NA
-  
-  # Calculate iVPD
-  
-  if(VPD >= 4100 & !is.na(VPD))
-    iVPD <- 0
-  if(VPD > 900 & VPD < 4100 & !is.na(VPD))
-    iVPD <- 1 - (VPD - 900)/(4100 - 900)
-  if(VPD <= 900 & !is.na(VPD))
-    iVPD <- 1
-  if(is.na(VPD))
-    iVPD <- NA
-  
-  # Calculate iPhoto
-  
-  if(Photoperiod <= 10 & !is.na(Photoperiod))
-    iPhoto <- 0
-  if(Photoperiod > 10 & Photoperiod < 11 & !is.na(Photoperiod))
-    iPhoto <- (Photoperiod - 10)/(11 - 10)
-  if(Photoperiod >= 11 & !is.na(Photoperiod))
-    iPhoto <- 1
-  if(is.na(Photoperiod))
-    iPhoto <- NA
+  get.es <- function(temp){
+    TempC <- (temp - 32) / 1.8
     
-  iGSI <- iTmin  * iVPD  * iPhoto
+    es <- 6.11 * exp((2.5e6 / 461) * (1 / 273 - 1 / (273 + TempC)))
+    return(es)
+  }
   
-  return(iGSI)
+  get.vpd <- function(rh, temp){
+    ## calculate saturation vapor pressure
+    es <- get.es(temp)
+    ## calculate vapor pressure deficit
+    vpd <- ((100 - rh) / 100) * es * 100
+    return(vpd)
+  }
+  
+  workingDF$VPD <- get.vpd(rh = workingDF$RH,
+                           temp = workingDF$Temp)
+  
+  iGSI_hourly <- function(MinimumTemp, VPD, Photoperiod){
+    
+    
+    # Calculate ITmin
+    
+    if(MinimumTemp >= 41 & !is.na(MinimumTemp))
+      iTmin <- 1
+    if(MinimumTemp < 41 & MinimumTemp > 28 & !is.na(MinimumTemp))
+      iTmin <- (MinimumTemp - 28)/(41 - 28)
+    if(MinimumTemp <= 28 & !is.na(MinimumTemp))
+      iTmin <- 0
+    if(is.na(MinimumTemp))
+      iTmin <- NA
+    
+    # Calculate iVPD
+    
+    if(VPD >= 4100 & !is.na(VPD))
+      iVPD <- 0
+    if(VPD > 900 & VPD < 4100 & !is.na(VPD))
+      iVPD <- 1 - (VPD - 900)/(4100 - 900)
+    if(VPD <= 900 & !is.na(VPD))
+      iVPD <- 1
+    if(is.na(VPD))
+      iVPD <- NA
+    
+    # Calculate iPhoto
+    
+    if(Photoperiod <= 10 & !is.na(Photoperiod))
+      iPhoto <- 0
+    if(Photoperiod > 10 & Photoperiod < 11 & !is.na(Photoperiod))
+      iPhoto <- (Photoperiod - 10)/(11 - 10)
+    if(Photoperiod >= 11 & !is.na(Photoperiod))
+      iPhoto <- 1
+    if(is.na(Photoperiod))
+      iPhoto <- NA
+    
+    iGSI <- iTmin  * iVPD  * iPhoto
+    
+    return(iGSI)
+  }
+  
+  workingDF_daily <- workingDF %>%
+    group_by(Year,Yday) %>%
+    summarise(Tmin = min(Temp, na.rm = TRUE), VPD = max(VPD, na.rm = TRUE), DayLength = max(DayLength), na.rm = TRUE) %>%
+    ungroup()
+  
+  workingDF_daily$GSI <- mapply(iGSI_hourly, workingDF_daily$Tmin, workingDF_daily$VPD, workingDF_daily$DayLength)
+  
+  workingDF_daily <- workingDF_daily %>%
+    group_by(Year,Yday) %>%
+    mutate(maxGSI = max(GSI, na.rm = TRUE))# %>% ungroup()
+  
+  workingDF_daily$rollGSI <- rollapply(data = workingDF_daily$maxGSI,  # original series
+                                       width = 21,  # width of the rolling window
+                                       FUN = mean, na.rm = T,  # Any arbitrary function
+                                       fill = NA)
+  
+  return(workingDF_daily)
+  
 }
 
+testGSIOutput <- calcGSI(DateTime = testWx_cleaned$DateTime,
+                         Temp = testWx_cleaned$Temp,
+                         RH = testWx_cleaned$RH,
+                         Latitude = as.numeric(stationMetadata$STATION$LATITUDE))
 
 
-test <- testWx_cleaned %>%
-  group_by(Year,yday(DateTime)) %>%
-  mutate(Tmin = min(Temp, na.rm = TRUE)) %>%
-  ungroup()
-
-View(test)
-
-test$GSI <- mapply(iGSI_hourly, test$Tmin, test$VPD, test$DayLength)
-
-test <- test %>%
-  group_by(Year,yday(DateTime)) %>%
-  mutate(maxGSI = max(GSI, na.rm = TRUE)) %>% ungroup()
-
-
-#test$rollGSI <- zoo::rollmean(x = test$maxGSI, k = 24*21, fill = list(NA,NULL,NA))
-test$rollGSI <- rollapply(data = test$maxGSI,  # original series
-                          width = 24 * 21,  # width of the rolling window
-                          FUN = mean, na.rm = T,  # Any arbitrary function
-                          fill = NA)
-  
-ggplot(data = test, aes(x = yday(DateTime), y = rollGSI))+
-  geom_point()+
-  facet_wrap(~Year)
-
-findGreenupDates <- function(DateTime, rollGSI){
-  workingDF <- data.frame(DateTime, rollGSI)
-  workingDF$Year <- year(DateTime)
+findGreenupDates <- function(Year, Yday,rollGSI){
+  workingDF <- data.frame(Year, Yday,rollGSI)
  
   greenup <- workingDF %>%
     group_by(Year) %>%
     filter(rollGSI >= 0.5) %>%
-    arrange(DateTime) %>%
+    arrange(Yday) %>%
     ungroup() %>%
     group_by(Year) %>%
-    summarise(GreenUpDate = min(DateTime))
+    summarise(GreenUpDate = min(Yday))
   
   senesence <- workingDF %>%
     group_by(Year) %>%
     filter(rollGSI >= 0.5) %>%
-    arrange(DateTime) %>%
+    arrange(Yday) %>%
     ungroup() %>%
     group_by(Year) %>%
-    summarise(SenesenceDate = max(DateTime))
+    summarise(SenesenceDate = max(Yday))
   
   growingSeason <- merge(greenup,senesence,by = "Year")
   
@@ -135,15 +143,19 @@ findGreenupDates <- function(DateTime, rollGSI){
     return(growingSeason)
 }
 
-seasonDF <- findGreenupDates(test$DateTime,test$rollGSI)
-#seasonDF$GreenUpDate <- 
+seasonDF <- findGreenupDates(Year = testGSIOutput$Year,
+                             Yday = testGSIOutput$Yday,
+                             testGSIOutput$rollGSI)
 
 
-ggplot(data = test, aes(x = yday(DateTime), y = rollGSI))+
+ggplot(data = testGSIOutput, aes(x = Yday, y = rollGSI))+
+  geom_line(color = "gray50", alpha = .5, aes(x = Yday, y = GSI))+
   geom_line(color = "black")+
-  geom_vline(data = seasonDF, color = "green", aes(xintercept = yday(seasonDF$GreenUpDate)))+
-  geom_vline(data = seasonDF, color = "red", aes(xintercept = yday(seasonDF$SenesenceDate)))+
+  geom_vline(data = seasonDF, color = "green", aes(xintercept = GreenUpDate))+
+  geom_vline(data = seasonDF, color = "red", aes(xintercept = SenesenceDate))+
   facet_wrap(~Year)
+
+
 
 liveFuelMoistures <- function(DateTime, GreenupDates, rollGSI){
   workingDF <- data.frame(DateTime,Year = year(DateTime),rollGSI)
